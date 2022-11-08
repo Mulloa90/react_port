ac963193fa7d2e4a9e849da5be800e38bbcc34b3
Mulloa90 <mario90gm@icloud.com>
2022-11-07 18:07:19 -0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import About from "./components/pages/Aboutpage";
+import Contact from "./components/pages/ContactPage";
+import Project from "./components/pages/ProjectPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <div>body here</div>
-    <Footer />
-    </>
-    
+    <Router>
+      <div className="page-wrapper">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route exact path="/about" element={<About/>}/>
+            <Route exact path="/projects" element={<Project/>}/>
+            <Route exact path="/contact" element={<Contact/>}/>
+            <Route path="*" element={<About/>}/>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/ProjectPage.jsx
@@ -0,0 +1,6 @@
+function Project() {
+    return <div> this is the project page</div>;
+  }
+  
+  export default Project;
+  
